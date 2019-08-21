331edd25e521ec8ee94aa32925e4102d75f8485e
Sigeno <>
2019-08-21 21:08:19 +0900

    Enabled to display in the list from input

--- view.py
+++ view.py
@@ -19,21 +19,26 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+@app.route("/post", methods=["POST"])
+def create():
+    context = request.form['context']
+    return redirect(url_for("index", matter=context))
+
+
 
 @app.route("/")
 def index():
-    now = datetime.date.today()
     month_current = datetime.date(year=2019,month=8, day=1)
     month_days = Calendar().monthdatescalendar(2019, 8)
-    month_before = datetime.date(year=2019, month=7, day=1)
-    month_next = datetime.date(year=2019, month=9, day=1)
+    context = request.args.get('matter','')
     week_name = ['月','火','水','木','金','土','日']
-
     return render_template(
-        "index.html",month_day = month_days, week_name = week_name, month_current = month_current.month
+        "index.html",month_day = month_days, week_name = week_name, month_current = month_current.month, context=context
     )
 
-
+@app.route("/<int:post_id>")
+def add_form(post_id):
+    return render_template("create.html")
 
 
 

06d423a6161fd2e3ab2b4f29e3537c89a0226214
Sigeno <>
2019-08-23 18:27:12 +0900

    yesterday work

--- view.py
+++ view.py
@@ -3,7 +3,7 @@ import calendar
 from calendar import Calendar
 import os
 import datetime
-
+import requests
 app = Flask(__name__)
 
 @app.context_processor
@@ -19,27 +19,39 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
-@app.route("/post", methods=["POST"])
-def create():
-    context = request.form['context']
-    return redirect(url_for("index", matter=context))
-
-
 
 @app.route("/")
 def index():
     month_current = datetime.date(year=2019,month=8, day=1)
     month_days = Calendar().monthdatescalendar(2019, 8)
     context = request.args.get('matter','')
+    get_date = request.args.get("day",'')
+    designated_date = datetime.datetime.strptime(get_date, "%Y-%m-%d").day
+    params = {
+        'context':  context,
+        'day': designated_date
+    }
     week_name = ['月','火','水','木','金','土','日']
+    
     return render_template(
-        "index.html",month_day = month_days, week_name = week_name, month_current = month_current.month, context=context
+        "index.html",month_day = month_days, week_name = week_name, month_current = month_current.month, params = params
     )
 
 @app.route("/<int:post_id>")
 def add_form(post_id):
-    return render_template("create.html")
+    return render_template("create.html", des_id=post_id)
 
+@app.route("/post", methods=["POST"])
+def create():
+    url = "http://localhost:3000/api/create"
+    params = {
+        "title": request.form['title'],
+        "context": request.form['context'],
+        "day": request.form['day']
+    }
+    json_request = requests.post(url, json=params)
+    print(json_request.status_code)
+    return redirect(url_for("index", matter=params['context'], day=params['day']))
 
 
 
--- view_api.py
+++ view_api.py
@@ -1,30 +1,43 @@
-from flask import Flask, jsonify, render_template, redirect, request
 
+from flask import Flask, jsonify, render_template, redirect, request, url_for
+import calendar
+from calendar import Calendar
+import os
+import datetime
 
 app = Flask(__name__)
 
-users = [
-    { "id": 1, "add_plan": "彼女とデート" },
-    { "id": 2, "add_plan": "試験対策" },
-    { "id": 3, "add_plan": "練習" },
-    {"id":4, "add_plan": "スペイン旅行"}
-]
+array_remind = {'title':[], 'content':[],'day':[]}
 
 
 @app.route("/api/index")
 def schedule_index():
-    index = users
-    pass
+    month_current = Calendar().monthdatescalendar(2019, 8)
+    return jsonify(month_current)
 
-
-@app.route("/api/add_plan", methods=["POST"])
+@app.route("/api/create", methods=["POST"])
 def create():
-    return jsonify(request.form)
+    pass
 
 
 @app.route("/api/detail", methods=["GET"])
 def detail():
     return jsonify(request.form)
+
+
+
+
+def db_remind(title, content, day):
+    array_remind['title'].append(title)
+    array_remind['content'].append(content)
+    array_remind['day'].append(day)
+    return array_remind
+
+
+
+
+
+
 
 if __name__ in "__main__":
     app.run(debug=True, threaded=True, port=3000, host="localhost")
